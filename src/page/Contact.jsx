7f596387addc0bb52c1d8688b10d09a8e363915a
Yulduzxon Maxammatyunusova <yunusovayulduz85@gmail.com>
2024-07-04 15:07:37 +0500

    contact page finished

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -20,50 +20,62 @@ const Contact = () => {
     <section className='contact'>
         <div className='container'>
           <h1 className='text-center text-[56px] font-bold border-b-2 border-[#F44749] py-4'>Contact Mahorat & Management</h1>
-          <div className='flex py-16'>
+          <div className='flex py-16 items-start'>
             <div className='w-4/12 flex flex-col justify-center items-center'>
               <div className='border rounded-full bg-[#2553B8] px-4 py-4'>
               <LocationSvg/>
               </div>
-              <p className='text-center font-light text-[19px]'><b className='font-semibold'>Address:</b> Safdosh Street, 10
+              <p className='text-center font-light text-[19px] py-3'><b className='font-semibold'>Address:</b> Safdosh Street, 10
                 Tashkent, Uzbekistan 100003</p>
             </div>
             <div className='w-4/12 flex flex-col justify-center items-center'>
               <div className='border rounded-full bg-[#2553B8] px-4 py-4'>
               <PhoneSvg/>
               </div>
-              <p className='text-center  text-[19px] font-semibold'>Phone: <a href="tel://998951950055" className='font-light text-[#2553B8] hover:underline'>+998 95 195-00-55</a> </p>
+              <p className='text-center  text-[19px] font-semibold py-3'>Phone: <a href="tel://998951950055" className='font-light text-[#2553B8] hover:underline'>+998 95 195-00-55</a> </p>
             </div>
             <div className='w-4/12 flex flex-col justify-center items-center'>
               <div className='border rounded-full bg-[#2553B8] px-4 py-4'>
               <MailSvg/>
               </div>
-              <p className='text-center  text-[19px] font-semibold'>Email: <a type='email' href="mailto:info@mahorat.org" className='font-light text-[#2553B8] hover:underline'>info@mahorat.org</a> </p>
+              <p className='text-center  text-[19px] font-semibold py-3'>Email: <a type='email' href="mailto:info@mahorat.org" className='font-light text-[#2553B8] hover:underline'>info@mahorat.org</a> </p>
             </div>
           </div>
-          <div className='flex items-center justify-between'>
-            <div className='bg-[#E8EDF0] py-5 px-5'>
+          <div className='flex items-center gap-4'>
+            <div className='bg-[#E8EDF0] py-5 px-5 w-7/12'>
               <form action="">
-                <h2 className='text-[42px] font-semibold'>Contact Us</h2>
+                <h2 className='text-[42px] font-semibold pb-4'>Contact Us</h2>
                 <div className='flex gap-4 items-center'>
                   <div>
-                  <label htmlFor="fullName">FULL NAME</label> <br />
-                  <input type="text" id='fullName' name='fullName' placeholder='FULL NAME'/>
+                  <label htmlFor="fullName" className='text-[12px] font-semibold'>FULL NAME</label> <br />
+                  <input type="text" className='rounded px-4 py-2 outline-none' id='fullName' name='fullName' placeholder='Full Name'/>
                   </div>
-                  <div>
-                  <label htmlFor="fullName">FULL NAME</label> <br />
-                  <input type="text" id='fullName' name='fullName' placeholder='FULL NAME'/>
+                  <div className='pt-1.5'>
+                    <label htmlFor="fullName" className='text-[12px] font-semibold'>EMAIL ADDRESS</label> <br />
+                    <input type="text" className='rounded px-4 py-2 outline-none' id='fullName' name='fullName' placeholder='Email Address'/>
                   </div>
                 </div>
+                <div className='py-2.5'>
+                  <label htmlFor="subject" className='text-[12px] font-semibold'>SUBJECT</label> <br />
+                  <input type="text" id='subject' name='subject' placeholder='Subject' className='w-full rounded px-4 py-2 outline-none' />
+                </div>
                 <div>
-                  <label htmlFor="subject">SUBJECT</label>
-                  <input type="text" placeholder='Subject' />
+                  <label className=' text-[12px] font-semibold' htmlFor="MESSAGE">MESSAGE</label> <br />
+                  <textarea placeholder='Message' className='w-full outline-none rounded px-4 py-2'/>
+                </div>
+                <div className='py-3'>
+                  <button className='bg-[#2553B8] text-white px-[16px] py-[12px] rounded text-[14px] font-normal hover:bg-[#5785E8] duration-300'>
+                    Send Message
+                  </button>
                 </div>
               </form>
             </div>
-              <div className='contact-img w-6/12 h-[500px]'></div>
+              <div className='contact-img w-5/12 h-[500px]'></div>
           </div>
         </div>
+          <div className='w-screen max-w-full pt-20'>
+            <iframe style={{width:'100%'}} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5762.635391208315!2d69.240903!3d41.277431!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8a8a65339a47%3A0x7be3a713adadc47b!2sSafdosh%20St%2C%20Tashkent%2C%20Uzbekistan!5e1!3m2!1sen!2sus!4v1720086303462!5m2!1sen!2sus" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
     </section>
     </div>
     
